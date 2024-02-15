688507139c5974cdcbe4f585b84fc285a613f432
Iliane Formet <iliane.formet@epita.fr>
2024-02-15 16:46:02 +0100

    change directory

--- FileExplorer/src/bash_commands/bash_commands.rs
+++ FileExplorer/src/bash_commands/bash_commands.rs
@@ -1,39 +1,45 @@
 use std::fs;
+use std::env;
 
+pub fn change_d(path:&str){
+    if let Err(erreur) = env::set_current_dir(path) {
+        eprintln!("Échec du changement de répertoire de travail : {}", erreur);
+        return;
+    }
+}
 
-// DIRECTORY
-pub fn create_dir(path:str, name:str){
-    let new_path = path + name;
+pub fn create_dir(path:&str, name:&str){
+    let new_path = format!("{}{}", path, name);
     match fs::create_dir(new_path) {
         Ok(_) => println!("Directory created successfully"),
         Err(err) => eprintln!("Error creating directory: {}", err),
     }
 }
 
-pub fn remove_dir(path:str, name:str){
-    let new_path = path + name;
+pub fn remove_dir(path:&str, name:&str){
+    let new_path = format!("{}{}", path, name);
     match fs::remove_dir_all(new_path) {
         Ok(_) => println!("Directory created successfully"),
         Err(err) => eprintln!("Error creating directory: {}", err),
     }
 }
 
-pub fn create_file(path:str, name:str){
+pub fn create_file(path:&str, name:&str){
     todo!();
 }
 
-pub fn remove_file(path:str, name:str){
-    let new_path = path + name;
+pub fn remove_file(path:&str, name:&str){
+    let new_path = format!("{}{}", path, name);
     match fs::remove_file(new_path) {
         Ok(_) => println!("Directory created successfully"),
         Err(err) => eprintln!("Error creating directory: {}", err),
     }
 }
 
-pub fn rename(path:str, name:str){
+pub fn rename(path:&str, name:&str){
     todo!();
 }
 
-pub fn metadata(path:str, name:str){
+pub fn metadata(path:&str, name:&str){
     todo!();
 }
--- FileExplorer/src/main.rs
+++ FileExplorer/src/main.rs
@@ -1,5 +1,16 @@
-use bash_commands;
+use FileExplorer::bash_commands::bash_commands::*;
 
 fn main() {
-    bash_commands::create_dir("/Users/ilianus/Desktop/EPITA/S4/PROJET/test")
+    let mut actual_dir = "/Users/ilianus/Desktop/EPITA/S4/PROJET/test";
+    create_dir(actual_dir, "/NOUVO");
+
+    // Changer le répertoire de travail
+    let nouveau_repertoire = "/Users/ilianus/Desktop/EPITA/S4/PROJET/test/NOUVO";
+    change_d(nouveau_repertoire);
+
+    actual_dir = "/Users/ilianus/Desktop/EPITA/S4/PROJET/test/NOUVO";
+
+    create_dir(actual_dir, "/NOUVO2");
+
+    dif
 }
